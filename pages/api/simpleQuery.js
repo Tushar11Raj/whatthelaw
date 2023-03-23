4862c953dd1ff798f8114821f7b7412fea133cbd
Tushar11Raj <tushar11raj@gmail.com>
2023-03-23 12:07:35 +0530

    commented everything in simpleQuery.js. Read it for reason

--- pages/api/simpleQuery.js
+++ pages/api/simpleQuery.js
@@ -1,103 +1,105 @@
-import { serve } from "https://deno.land/std@0.170.0/http/server.ts";
-import "https://deno.land/x/xhr@0.2.1/mod.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.5.0";
-import GPT3Tokenizer from "https://esm.sh/gpt3-tokenizer@1.1.5";
-import { Configuration, OpenAIApi } from "https://esm.sh/openai@3.1.0";
-import { stripIndent } from "https://esm.sh/common-tags@1.8.2";
-import { supabaseClient } from "@/util/supabaseClient";
+//commented this whole segment. as vercel webpack does not support importing from URIs
 
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type",
-};
+// import { serve } from "https://deno.land/std@0.170.0/http/server.ts";
+// import "https://deno.land/x/xhr@0.2.1/mod.ts";
+// import { createClient } from "https://esm.sh/@supabase/supabase-js@2.5.0";
+// import GPT3Tokenizer from "https://esm.sh/gpt3-tokenizer@1.1.5";
+// import { Configuration, OpenAIApi } from "https://esm.sh/openai@3.1.0";
+// import { stripIndent } from "https://esm.sh/common-tags@1.8.2";
+// import { supabaseClient } from "@/util/supabaseClient";
 
-serve(async (req) => {
-  // Handle CORS
-  if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
-  }
+// export const corsHeaders = {
+//   "Access-Control-Allow-Origin": "*",
+//   "Access-Control-Allow-Headers":
+//     "authorization, x-client-info, apikey, content-type",
+// };
 
-  // Search query is passed in request payload
-  const { query } = await req.json();
+// serve(async (req) => {
+//   // Handle CORS
+//   if (req.method === "OPTIONS") {
+//     return new Response("ok", { headers: corsHeaders });
+//   }
 
-  // OpenAI recommends replacing newlines with spaces for best results
-  const input = query.replace(/\n/g, " ");
+//   // Search query is passed in request payload
+//   const { query } = await req.json();
 
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+//   // OpenAI recommends replacing newlines with spaces for best results
+//   const input = query.replace(/\n/g, " ");
 
-  // Generate a one-time embedding for the query itself
-  const embeddingResponse = await openai.createEmbedding({
-    model: "text-embedding-ada-002",
-    input,
-  });
+//   const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+//   });
+//   const openai = new OpenAIApi(configuration);
 
-  const [{ embedding }] = embeddingResponse.data.data;
+//   // Generate a one-time embedding for the query itself
+//   const embeddingResponse = await openai.createEmbedding({
+//     model: "text-embedding-ada-002",
+//     input,
+//   });
 
-  // Fetching whole documents for this simple example.
-  //
-  // Ideally for context injection, documents are chunked into
-  // smaller sections at earlier pre-processing/embedding step.
-  const { data: documents } = await supabaseClient.rpc("match_documents", {
-    query_embedding: embedding,
-    match_threshold: 0.78, // Choose an appropriate threshold for your data
-    match_count: 10, // Choose the number of matches
-  });
+//   const [{ embedding }] = embeddingResponse.data.data;
 
-  const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
-  let tokenCount = 0;
-  let contextText = "";
+//   // Fetching whole documents for this simple example.
+//   //
+//   // Ideally for context injection, documents are chunked into
+//   // smaller sections at earlier pre-processing/embedding step.
+//   const { data: documents } = await supabaseClient.rpc("match_documents", {
+//     query_embedding: embedding,
+//     match_threshold: 0.78, // Choose an appropriate threshold for your data
+//     match_count: 10, // Choose the number of matches
+//   });
 
-  // Concat matched documents
-  for (let i = 0; i < documents.length; i++) {
-    const document = documents[i];
-    const content = document.content;
-    const encoded = tokenizer.encode(content);
-    tokenCount += encoded.text.length;
+//   const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
+//   let tokenCount = 0;
+//   let contextText = "";
 
-    // Limit context to max 1500 tokens (configurable)
-    if (tokenCount > 1500) {
-      break;
-    }
+//   // Concat matched documents
+//   for (let i = 0; i < documents.length; i++) {
+//     const document = documents[i];
+//     const content = document.content;
+//     const encoded = tokenizer.encode(content);
+//     tokenCount += encoded.text.length;
 
-    contextText += `${content.trim()}\n---\n`;
-  }
+//     // Limit context to max 1500 tokens (configurable)
+//     if (tokenCount > 1500) {
+//       break;
+//     }
 
-  const prompt = stripIndent`${oneLine`
-    You are a very enthusiastic Supabase representative who loves
-    to help people! Given the following sections from the Supabase
-    documentation, answer the question using only that information,
-    outputted in markdown format. If you are unsure and the answer
-    is not explicitly written in the documentation, say
-    "Sorry, I don't know how to help with that."`}
+//     contextText += `${content.trim()}\n---\n`;
+//   }
 
-    Context sections:
-    ${contextText}
+//   const prompt = stripIndent`${oneLine`
+//     You are a very enthusiastic Supabase representative who loves
+//     to help people! Given the following sections from the Supabase
+//     documentation, answer the question using only that information,
+//     outputted in markdown format. If you are unsure and the answer
+//     is not explicitly written in the documentation, say
+//     "Sorry, I don't know how to help with that."`}
 
-    Question: """
-    ${query}
-    """
+//     Context sections:
+//     ${contextText}
 
-    Answer as markdown (including related code snippets if available):
-  `;
+//     Question: """
+//     ${query}
+//     """
 
-  // In production we should handle possible errors
-  const completionResponse = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-    max_tokens: 512, // Choose the max allowed tokens in completion
-    temperature: 0, // Set to 0 for deterministic results
-  });
+//     Answer as markdown (including related code snippets if available):
+//   `;
 
-  const {
-    id,
-    choices: [{ text }],
-  } = completionResponse.data;
+//   // In production we should handle possible errors
+//   const completionResponse = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt,
+//     max_tokens: 512, // Choose the max allowed tokens in completion
+//     temperature: 0, // Set to 0 for deterministic results
+//   });
 
-  return new Response(JSON.stringify({ id, text }), {
-    headers: { ...corsHeaders, "Content-Type": "application/json" },
-  });
-});
+//   const {
+//     id,
+//     choices: [{ text }],
+//   } = completionResponse.data;
+
+//   return new Response(JSON.stringify({ id, text }), {
+//     headers: { ...corsHeaders, "Content-Type": "application/json" },
+//   });
+// });
